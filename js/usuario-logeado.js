d2ae8c125e4143d3e92a2bf7d22fa0e6642762b6
LeandroLeiton33 <144855412+LeandroLeiton33@users.noreply.github.com>
2023-10-24 01:19:45 -0300

    tercer commit

--- js/usuario-logeado.js
+++ js/usuario-logeado.js
@@ -1,64 +1,59 @@
-const usuarioLogeado = JSON.parse(localStorage.getItem("usuarioLogeado"))
-//Obtenemos elementos HTML
-const headerUserName = document.getElementById("user-header-name")
-const headerUserAction = document.getElementById("btn-action")
-const navbarLink = document.querySelector("ul.navbar-nav#nav-list")
+const usuarioLogeado = JSON.parse(localStorage.getItem("usuarioLogeado"));
+// Obtenemos elementos HTML
+const headerUserName = document.getElementById("user-header-name");
+const headerUserAction = document.getElementById("btn-action");
+const navbarList = document.querySelector("ul.navbar-nav#nav-list");
 
+headerUserName.innerText = usuarioLogeado ? usuarioLogeado.nombreCompleto : "";
 
-headerUserName.innerText = usuarioLogeado ? usuarioLogeado.nombreCompleto : ""
+if (usuarioLogeado) {
+    headerUserAction.innerHTML = `<button class="btn btn-danger" onclick="logout()">Logout</button>`;
+    
+    // Checkear si el usuario logueado es ADMIN
+    if (usuarioLogeado.rol === 'admin') {
+        // Deberíamos pintar dichos botones
 
+        const btnAdminProduct = document.createElement('li');
+        btnAdminProduct.classList.add('nav-item');
+        btnAdminProduct.id = 'nav-admin-product';
 
-if(usuarioLogeado) {
-    headerUserAction.innerHTML = `<button class="btn btn-danger" onclick="logout()">Logout</button>`
-    //Checkear si el user logueado es ADMIN
-    if(usuarioLogeado.rol === 'admin') {
-        //Deberíamos pintar dichos botones
+        const btnAdminUsers = document.createElement('li'); // Crear el botón para Admin Usuarios
+        btnAdminUsers.classList.add('nav-item');
+        btnAdminUsers.id = 'nav-admin-users';
 
-        const btnAdminProduct = document.createElement('li')
-        btnAdminProduct.classList.add('nav-item')
-        btnAdminProduct.id = 'nav-admin-product'
+        const linkAdminProduct = document.createElement('a');
+        linkAdminProduct.classList.add('nav-link');
+        linkAdminProduct.href = '/pages/admin.html';
+        linkAdminProduct.innerText = 'Admin Productos';
 
-        // Si necesito agregarle la clase  active al botón
+        const linkAdminUsers = document.createElement('a'); // Crear el enlace para Admin Usuarios
+        linkAdminUsers.classList.add('nav-link');
+        linkAdminUsers.href = '/pages/usuarios.html';
+        linkAdminUsers.innerText = 'Admin Usuarios';
+
+        // Si necesitas agregarle la clase "active" a los botones
         const url = window.location.pathname;
-        if(url.includes('admin.html')) {
 
-            btnAdminProduct.classList.add('active')
+        if (url.includes('admin.html')) {
+            btnAdminProduct.classList.add('active');
+        } else if (url.includes('usuarios.html')) {
+            btnAdminUsers.classList.add('active');
         }
-        const link = document.createElement('a')
-        link.classList.add('nav-link',)
-        link.href = '/pages/admin.html';
-        link.innerText = 'Admin Productos'
 
-        const url2 = window.location.pathname;
-        if(url2.includes('usuarios.html')) {
+        btnAdminProduct.appendChild(linkAdminProduct);
+        btnAdminUsers.appendChild(linkAdminUsers);
 
-            btnAdminProduct.classList.add('active')
-        }
-        const linkUser = document.createElement('a')
-        linkUser.classList.add('nav-link',)
-        linkUser.href= '/pages/usuarios.html';
-        linkUser.innerText = 'Admin Usuarios'
-        
-
-
-
-        btnAdminProduct.appendChild(link)
-
-        navbarLink.appendChild(btnAdminProduct)
-
+        // Agregar ambos botones al menú de navegación
+        navbarList.appendChild(btnAdminProduct);
+        navbarList.appendChild(btnAdminUsers);
     }
-
-
 } else {
-    headerUserAction.innerHTML = `<a class="btn btn-dark" href="/pages/login.html">Login</a>`
+    headerUserAction.innerHTML = `<a class="btn btn-primary" href="/pages/login.html">Login</a>`;
 }
 
-
-
-
 function logout() {
-    localStorage.removeItem("usuarioLogeado")
-    setTimeout(function() {
-        window.location.href = "/index.html"
-    }, 500)
+    localStorage.removeItem("usuarioLogeado");
+    setTimeout(function () {
+        window.location.href = "/index.html";
+    }, 500);
 }
